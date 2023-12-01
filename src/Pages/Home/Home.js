1dedaa2b84b5f9b20f7e99e93d6b8d3fa327f5d4
Martin wichstrøm jørgensen <mwj@Martin-MacBook-Pro.local>
2023-12-01 17:45:02 +0100

    new linkedin url

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -14,7 +14,7 @@ function Home() {
         <section class="Links">
             <p data-item='Reach me @'>Reach me @</p>
             <a href="https://github.com/Martiwj" target="_blank">GitHub</a>
-            <a href="https://www.linkedin.com/authwall?trk=bf&trkInfo=AQHpD7UhPZ5VCwAAAYwmPrUIO1b52bqc4t3B6_AMiiQ1mLtWsMKxn_aTQZFFbFWj4l6ruXSvzJF1IdzNJ9xrBXZtOiTbt-J9h07Z7ai1okMzphu3c8uP3_3qj_4T7HRGl6XTvrM=&original_referer=&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fmartin-wichstrom-jorgensen" target="_blank">LinkedIn</a>
+            <a href="www.linkedin.com/in/martiwj" target="_blank">LinkedIn</a>
             <a href="mailto:martiwj@uio.no" target="_blank" title="Send e-post til Martin">Mail</a>
         </section>
     </div>
