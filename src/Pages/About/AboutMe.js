59ffe02e2e8014cd33eee7374085350c0e0eb57c
Martin wichstrøm jørgensen <mwj@Martin-MacBook-Pro.local>
2024-02-12 13:21:41 +0100

    Minor fix on aboutme page

--- src/Pages/About/AboutMe.js
+++ src/Pages/About/AboutMe.js
@@ -16,7 +16,7 @@ function AboutMe() {
         <div className="text-container">
           <h1>Martin W Jørgensen</h1>
           <p>
-            I am studying for a bachelor's degree in computer science, currently in my 2nd year at the University of Oslo. I have a strong passion for algorithms and data structures and love to challenge myself with intricate problem-solving.
+            I am studying for a bachelor's degree in informatics currently in my 2nd year at the University of Oslo. I have a strong passion for algorithms and data structures and love to challenge myself with intricate problem-solving.
           </p>
         </div>
         <img src="/portrett.png" alt="" />
