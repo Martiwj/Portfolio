4c9c3521e1aa7ef6d44fb539d3a6854410fc82c7
Martiwj <Martin.wich.jorg@gmail.com>
2024-08-05 16:30:25 +0200

    Hot fix; correct email

--- src/Pages/Home/Homesite.js
+++ src/Pages/Home/Homesite.js
@@ -15,7 +15,7 @@ function Home() {
             <p className="animation" data-item='Reach me @'>Reach me @</p>
             <a href="https://github.com/Martiwj" target="_blank" rel="noreferrer">GitHub</a>
             <a href="https://www.linkedin.com/in/martiwj" target="_blank" rel="noreferrer">LinkedIn</a>
-            <a href="mailto:martiwj@.ifi.uio.no" target="_blank" title="Send e-post til Martin" rel="noreferrer">Mail</a>
+            <a href="mailto:martiwj@ifi.uio.no" target="_blank" title="Send e-post til Martin" rel="noreferrer">Mail</a>
             <a href="https://open.kattis.com/users/mj" target="_blank" rel="noreferrer">Kattis</a>
             <a href="https://wakatime.com/@HissigKoder?rank=me&page=1&board=b98c1a35-338e-4e50-90ec-4d1c1bdf7e80" target="_blank" rel="noreferrer">WakaTime</a>
         </section>
